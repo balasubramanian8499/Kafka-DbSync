148d333eb90f3857c749454f479105436fa8805a
Balasubramanian S <86341262+balasubramanian8499@users.noreply.github.com>
2024-10-02 15:40:45 +0530

    Adding kafka-sync-consumer/src/main/java/com/spring/kafka_sync_consumer/KafkaSyncConsumerApplication.java

---
+++ kafka-sync-consumer/src/main/java/com/spring/kafka_sync_consumer/KafkaSyncConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.spring.kafka_sync_consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaSyncConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaSyncConsumerApplication.class, args);
+	}
+
+}
