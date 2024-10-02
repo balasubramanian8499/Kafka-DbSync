cdc305c9a402f14167050c7ae46c062790abc55d
Balasubramanian S <86341262+balasubramanian8499@users.noreply.github.com>
2024-10-02 15:41:06 +0530

    Adding kafka-sync-producer/src/test/java/com/spring/kafka_sync_producer/KafkaSyncProducerApplicationTests.java

---
+++ kafka-sync-producer/src/test/java/com/spring/kafka_sync_producer/KafkaSyncProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.kafka_sync_producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaSyncProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
