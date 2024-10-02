07015d1f4c33557177aee853520db5d1cad40d26
Balasubramanian S <86341262+balasubramanian8499@users.noreply.github.com>
2024-10-02 15:40:46 +0530

    Adding kafka-sync-consumer/src/main/java/com/spring/kafka_sync_consumer/model/User.java

---
+++ kafka-sync-consumer/src/main/java/com/spring/kafka_sync_consumer/model/User.java
@@ -0,0 +1,39 @@
+package com.spring.kafka_sync_consumer.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.sql.Timestamp;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    public Integer id;
+
+    @Column(name = "user_name")
+    private String userName;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @Column(name = "created_at")
+    @CreationTimestamp
+    private Timestamp createdAt;
+
+    @Column(name = "modified_at")
+    @UpdateTimestamp
+    private Timestamp modifiedAt;
+
+}
