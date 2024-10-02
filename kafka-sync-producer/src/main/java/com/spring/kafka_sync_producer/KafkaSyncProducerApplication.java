7e79b9d8944efcff5b9454cc8460b37ead5f623a
Balasubramanian S <86341262+balasubramanian8499@users.noreply.github.com>
2024-10-02 15:41:02 +0530

    Adding kafka-sync-producer/src/main/java/com/spring/kafka_sync_producer/KafkaSyncProducerApplication.java

---
+++ kafka-sync-producer/src/main/java/com/spring/kafka_sync_producer/KafkaSyncProducerApplication.java
@@ -0,0 +1,13 @@
+package com.spring.kafka_sync_producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaSyncProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaSyncProducerApplication.class, args);
+	}
+
+}
